2faeaa25013775db44e0f443698064cb1dc52bdf
jurajjasik <juraj.jasik@gmail.com>
2024-08-24 19:00:51 +0200

    implementing

--- keithley6517b-mqtt-gui/keithley6517b_mqtt_client_logic.py
+++ keithley6517b-mqtt-gui/keithley6517b_mqtt_client_logic.py
@@ -1,13 +1,39 @@
+import json
 import logging
 
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import CallbackAPIVersion
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QObject, pyqtSignal
 
 logger = logging.getLogger(__name__)
 
 
+# decorator to check if the client is connected
+def client_connected(func):
+    def wrapper(self, *args, **kwargs):
+        if not self.client.is_connected():
+            logger.warning("Client is not connected")
+            return
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+# decorator to log the function call
+def log_func(func):
+    def wrapper(*args, **kwargs):
+        logger.debug(f"Calling {func.__name__} with args: {args}, kwargs: {kwargs}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
 class Keithley6517B_MQTTClientLogic(QObject):
+    signal_current = pyqtSignal(float)
+    signal_state = pyqtSignal(object)
+    device_status_changed = pyqtSignal(str)
+    mqtt_status_changed = pyqtSignal(str)
+
     def __init__(self, config):
         super().__init__()
 
@@ -37,12 +63,127 @@ class Keithley6517B_MQTTClientLogic(QObject):
         logger.debug(f"Connected with result code {rc}")
 
         self.client.subscribe(f"{self.topic_base}/response/{self.device_name}/#")
-
         self.client.subscribe(f"{self.topic_base}/connected/{self.device_name}")
+        self.client.subscribe(f"{self.topic_base}/error/{self.device_name}")
 
-        self.client.subscribe(f"{self.topic_base}/connected/{self.device_name}")
+        self.mqtt_status_changed.emit(
+            f"connected to broker {self.config['mqtt_broker']}:{self.config['mqtt_port']}"
+        )
 
     def on_message(self, client, userdata, message):
-        print(
-            f"Received message '{message.payload.decode()}' on topic '{message.topic}'"
+        topic = message.topic
+        try:
+            payload = json.loads(message.payload.decode())
+        except json.JSONDecodeError as e:
+            logger.debug(f"Error decoding message payload: {e}")
+            payload = {}
+
+        logger.debug(f"Received message on topic {topic} with payload {payload}")
+
+        if "/connected/" in topic:
+            self.handle_device_connected(message)
+        elif "/error/" in topic:
+            self.handle_device_error(message)
+        elif topic.endswith("current"):
+            self.handle_current(payload)
+        elif topic.endswith("current_range"):
+            self.handle_current_range(payload)
+        elif topic.endswith("source_voltage"):
+            self.handle_source_voltage(payload)
+        elif topic.endswith("source_enable"):
+            self.handle_source_voltage_enable(payload)
+
+    def handle_device_connected(self, message):
+        logger.info("Device connected")
+        self.device_status_changed.emit("connected")
+
+    def handle_device_error(self, message):
+        logger.warning(f"Device error: {message.payload}")
+        self.device_status_changed.emit("IO error")
+
+    def handle_current(self, payload):
+        if "value" in payload:
+            try:
+                current = float(payload["value"])
+                self.signal_current.emit(current)
+            except Exception as e:
+                logger.warning(f"Error handling current message: {e}")
+
+    def handle_current_range(self, payload):
+        if "value" in payload:
+            try:
+                current_range = float(payload["value"])
+                logger.debug(f"Current range: {current_range}")
+                self.signal_state.emit({"current_range": current_range})
+            except Exception as e:
+                logger.warning(f"Error handling current range message: {e}")
+
+    def handle_source_voltage(self, payload):
+        if "value" in payload:
+            try:
+                voltage = float(payload["value"])
+                logger.debug(f"Source voltage: {voltage}")
+                self.signal_state.emit({"source_voltage": voltage})
+            except Exception as e:
+                logger.warning(f"Error handling source voltage message: {e}")
+
+    def handle_source_voltage_enable(self, payload):
+        if "value" in payload:
+            try:
+                enable = bool(payload["value"])
+                logger.debug(f"Source voltage enable: {enable}")
+                self.signal_state.emit({"source_voltage_enable": enable})
+            except Exception as e:
+                logger.warning(f"Error handling source voltage enable message: {e}")
+
+    @client_connected
+    @log_func
+    def publish_source_voltage(self, voltage):
+        self.client.publish(
+            topic=f"{self.topic_base}/cmnd/{self.device_name}/source_voltage",
+            payload=json.dumps({"value": voltage}),
+        )
+
+    @client_connected
+    @log_func
+    def publish_source_voltage_enable(self, enable):
+        self.client.publish(
+            topic=f"{self.topic_base}/cmnd/{self.device_name}/source_enabled",
+            payload=json.dumps({"value": enable}),
+        )
+
+    @client_connected
+    @log_func
+    def publish_shutdown(self):
+        self.client.publish(
+            topic=f"{self.topic_base}/cmnd/{self.device_name}/shutdown",
+        )
+
+    @client_connected
+    @log_func
+    def publish_reset(self):
+        self.client.publish(
+            topic=f"{self.topic_base}/cmnd/{self.device_name}/reset",
+        )
+
+    @client_connected
+    @log_func
+    def publish_measure(self, nplc, current, auto_range):
+        self.client.publish(
+            topic=f"{self.topic_base}/cmnd/{self.device_name}/measure_current",
+            payload=json.dumps(
+                {
+                    "nplc": nplc,
+                    "current": current,
+                    "auto_range": auto_range,
+                }
+            ),
+        )
+
+    @client_connected
+    @log_func
+    def publish_current_range(self, value):
+        self.client.publish(
+            topic=f"{self.topic_base}/cmnd/{self.device_name}/current_range",
+            payload=json.dumps({"value": value}),
         )
--- keithley6517b-mqtt-gui/keithley6517b_mqtt_gui.py
+++ keithley6517b-mqtt-gui/keithley6517b_mqtt_gui.py
@@ -3,6 +3,7 @@ import logging
 import os
 import time
 
+from engineering_notation import EngNumber
 from PyQt5.QtCore import Qt, QThread, QTimer, pyqtSignal
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QWidget  # Add this line to import QWidget
@@ -36,6 +37,25 @@ from PyQt5.QtWidgets import (
 )
 
 from .keithley6517b_mqtt_client_logic import Keithley6517B_MQTTClientLogic
+
+
+def speed_to_nplc(speed):
+    if speed == "Fast":
+        return 0.01
+    elif speed == "Medium":
+        return 0.1
+    elif speed == "Slow":
+        return 1
+    elif speed == "High Accuracy":
+        return 10
+    else:
+        return 0.1
+
+
+def text_to_current_range(text):
+    auto_range = True if text == "Auto" else False
+    current_range = float(EngNumber(text)) if not auto_range else 0
+    return auto_range, current_range
 
 
 # Initialize status bar with pernament widgets
@@ -79,16 +99,31 @@ class Keithley6517B_MQTT_GUI(QMainWindow):
     def __init__(self, config):
         super().__init__()
 
+        self.client_logic = Keithley6517B_MQTTClientLogic(config)
+
         self.setWindowTitle("Keithley 6517B MQTT GUI")
         self.setWindowIcon(QIcon("icon.png"))
 
         self.init_ui()
 
-        self.client_logic = Keithley6517B_MQTTClientLogic(config)
         self.client_logic.start()
 
     def init_ui(self):
         # Create widgets
+
+        # Measured current label
+        self.current_label = QLabel("000.0000 fA")
+        self.current_label.setStyleSheet("font-size: 48pt; font-weight: bold;")
+
+        # Shutdown button
+        self.shutdown_button = QPushButton("Shutdown")
+
+        # Reset button
+        self.reset_button = QPushButton("Reset")
+
+        # Measure button
+        self.measure_button = QPushButton("Measure Continuously")
+
         # Source Voltage Control
         self.source_voltage_label = QLabel("Source Voltage (V)")
         self.source_voltage_ctrl = QDoubleSpinBox()
@@ -96,13 +131,42 @@ class Keithley6517B_MQTT_GUI(QMainWindow):
         self.source_voltage_ctrl.setSingleStep(0.1)
         self.source_voltage_ctrl.setValue(0.0)
 
+        # Source Voltage Enable Checkbox
+        self.source_voltage_enable = QCheckBox("Enable Source Voltage")
+        self.source_voltage_enable.setChecked(False)
+
+        # Current range control
+        self.current_range_label = QLabel("Current Range (A)")
+        self.current_range_ctrl = QComboBox()
+        self.current_range_ctrl.addItems(["Auto", "1p", "1n", "1u", "1m"])
+
+        # Speed control
+        self.speed_label = QLabel("Speed")
+        self.speed_ctrl = QComboBox()
+        self.speed_ctrl.addItems(["Fast", "Medium", "Slow", "High Accuracy"])
+
         # Create grid layout
         self.grid = QGridLayout()
         self.grid.setSpacing(10)
 
         # add widgets to grid
-        self.grid.addWidget(self.source_voltage_label, 0, 0)
-        self.grid.addWidget(self.source_voltage_ctrl, 0, 1)
+        self.grid.addWidget(self.current_label, 0, 0, 2, 3)
+
+        self.grid.addWidget(self.shutdown_button, 2, 0)
+        self.grid.addWidget(self.reset_button, 2, 1)
+        self.grid.addWidget(self.measure_button, 2, 2)
+
+        self.grid.addWidget(self.source_voltage_label, 3, 0)
+        self.grid.addWidget(self.source_voltage_ctrl, 3, 1)
+        self.grid.addWidget(self.source_voltage_enable, 3, 2)
+
+        self.grid.addWidget(self.current_range_label, 4, 0)
+        self.grid.addWidget(self.current_range_ctrl, 4, 1)
+
+        self.grid.addWidget(self.speed_label, 5, 0)
+        self.grid.addWidget(self.speed_ctrl, 5, 1)
+
+        
 
         # Create central widget
         central_widget = QWidget()
@@ -116,6 +180,118 @@ class Keithley6517B_MQTT_GUI(QMainWindow):
 
         # attach signals
         self.source_voltage_ctrl.valueChanged.connect(self.on_voltage_input_changed)
+        self.source_voltage_enable.stateChanged.connect(
+            self.on_source_voltage_enable_changed
+        )
+        self.shutdown_button.clicked.connect(self.on_shutdown_button_clicked)
+        self.reset_button.clicked.connect(self.on_reset_button_clicked)
+        self.measure_button.clicked.connect(self.on_measure_button_clicked)
+        self.current_range_ctrl.currentIndexChanged.connect(
+            self.on_current_range_changed
+        )
+        self.speed_ctrl.currentIndexChanged.connect(self.on_speed_changed)
+
+        self.client_logic.device_status_changed.connect(self.on_device_status_changed)
+        self.client_logic.mqtt_status_changed.connect(self.on_mqtt_status_changed)
+        self.client_logic.signal_current.connect(self.on_measured_current_changed)
+        self.client_logic.signal_state.connect(self.on_state_changed)
+
+    def publish_measure(self):
+        nplc = speed_to_nplc(self.speed_ctrl.currentText())
+        auto_range, current_range = text_to_current_range(
+            self.current_range_ctrl.currentText()
+        )
+        self.client_logic.publish_measure(nplc, current_range, auto_range)
 
     def on_voltage_input_changed(self, value):
         self.client_logic.publish_source_voltage(value)
+
+    def on_source_voltage_enable_changed(self, state):
+        self.client_logic.publish_source_voltage_enable(
+            self.source_voltage_enable.isChecked()
+        )
+
+    def on_shutdown_button_clicked(self):
+        self.client_logic.publish_shutdown()
+
+    def on_reset_button_clicked(self):
+        self.client_logic.publish_reset()
+
+    def on_measure_button_clicked(self):
+        self.publish_measure()
+
+    def on_current_range_changed(self, index):
+        self.publish_measure()
+
+    def on_speed_changed(self, index):
+        self.publish_measure()
+
+    def on_device_status_changed(self, status):
+        self.status_bar_logic.set_device_status(status)
+
+    def on_mqtt_status_changed(self, status):
+        self.status_bar_logic.set_mqtt_status(status)
+
+    def on_measured_current_changed(self, current):
+        current_str = str(EngNumber(current, precision=4))
+        self.current_label.setText(current_str)
+
+    def on_state_changed(self, state):
+        if "source_voltage" in state:
+            # disable signal to prevent infinite loop
+            self.source_voltage_ctrl.valueChanged.disconnect(
+                self.on_voltage_input_changed
+            )
+            self.source_voltage_ctrl.setValue(state["source_voltage"])
+            self.source_voltage_ctrl.valueChanged.connect(self.on_voltage_input_changed)
+
+        if "source_voltage_enable" in state:
+            # disable signal to prevent infinite loop
+            self.source_voltage_enable.stateChanged.disconnect(
+                self.on_source_voltage_enable_changed
+            )
+            self.source_voltage_enable.setChecked(state["source_voltage_enable"])
+            self.source_voltage_enable.stateChanged.connect(
+                self.on_source_voltage_enable_changed
+            )
+
+        if "current_range" in state:
+            # disable signal to prevent infinite loop
+            self.current_range_ctrl.currentIndexChanged.disconnect(
+                self.on_current_range_changed
+            )
+            self.current_range_ctrl.setCurrentText(state["current_range"])
+            self.current_range_ctrl.currentIndexChanged.connect(
+                self.on_current_range_changed
+            )
+
+        if "nplc" in state:
+            nplc = state["nplc"]
+            speed = (
+                "Fast"
+                if nplc == 0.01
+                else (
+                    "Medium"
+                    if nplc == 0.1
+                    else "Slow" if nplc == 1 else "High Accuracy"
+                )
+            )
+            # disable signal to prevent infinite loop
+            self.speed_ctrl.currentIndexChanged.disconnect(self.on_speed_changed)
+            self.speed_ctrl.setCurrentText(speed)
+            self.speed_ctrl.currentIndexChanged.connect(self.on_speed_changed)
+
+        if "auto_range" in state:
+            auto_range = state["auto_range"]
+            # disable signal to prevent infinite loop
+            self.current_range_ctrl.currentIndexChanged.disconnect(
+                self.on_current_range_changed
+            )
+            self.current_range_ctrl.setCurrentText(
+                "Auto"
+                if auto_range
+                else str(state["current_range"]) if "current_range" in state else "Auto"
+            )
+            self.current_range_ctrl.currentIndexChanged.connect(
+                self.on_current_range_changed
+            )
