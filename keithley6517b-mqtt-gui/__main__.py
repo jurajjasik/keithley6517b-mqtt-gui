50495ab486119a8ba043f0e3545b9b1be857d761
jurajjasik <juraj.jasik@gmail.com>
2024-08-24 14:12:26 +0200

    Update __main__.py

--- keithley6517b-mqtt-gui/__main__.py
+++ keithley6517b-mqtt-gui/__main__.py
@@ -1,24 +1,31 @@
 # GUI applicaption to control Keithley 6517B electrometer over MQTT.
 
-import PyQt5.QtWidgets as QtWidgets
-import PyQt5.QtCore as QtCore
-import PyQt5.QtGui as QtGui
-
 import sys
 
-from keithley6517b_mqtt_gui import Keithley6517B_MQTT_GUI
+import PyQt5.QtCore as QtCore
+import PyQt5.QtGui as QtGui
+import PyQt5.QtWidgets as QtWidgets
+import yaml
 
-def main():
+from .keithley6517b_mqtt_gui import Keithley6517B_MQTT_GUI
+
+
+def main(config_file):
     app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName("Keithley 6517B MQTT GUI")
     app.setApplicationVersion("0.1.0")
-    app.setOrganizationName("CERN")
-    app.setOrganizationDomain("cern.ch")
+    app.setOrganizationName("JHI")
+    app.setOrganizationDomain("jh-inst.cas.cz")
 
-    window = Keithley6517B_MQTT_GUI()
+    with open(config_file, "r") as f:
+        config = yaml.safe_load(f)
+
+    window = Keithley6517B_MQTT_GUI(config)
     window.show()
 
     sys.exit(app.exec_())
 
+
 if __name__ == "__main__":
-    main()
+    config_file = sys.argv[1] if len(sys.argv) > 1 else "config.yaml"
+    main(config_file)
